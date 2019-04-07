4f86d4effac47f3f0ea1cdd9f3df5f381d63d588
Natalia Sochina <natalia.sochina@gmail.com>
2019-04-07 18:15:36 +0300

    Ex2 Test for Wiki Android
    Ex2. test for Wiki, if cannot find Search… faild

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -3,7 +3,12 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -33,6 +38,44 @@ public class FirstTest {
     }
     @Test
     public void FirstTest(){
-        System.out.println("FirstTest starts");
+
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cannot find SearchLine",
+                5);
+
+        waitForElementPresent(
+                By.xpath("//*[contains(@text,'Search…')]"),
+                "Cannot find text 'Search…'",
+                15
+        );
+
+
+//        WebElement element_to_init_search = driver.findElementByXPath("//*[contains(@text,'Search Wikipedia')]");
+//        element_to_init_search.click();
+//        WebElement elemet_to_enter_search_line = driver.findElementById("");
+//       elemet_to_enter_search_line.sendKeys(""Java);
+
+
     }
+    private WebElement waitForElementPresent(By by,String error_message, long timoutInSec)
+    {
+        WebDriverWait wait = new WebDriverWait(driver, timoutInSec);
+        wait.withMessage(error_message+"\n");
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by)
+        );
+    }
+    private WebElement waitForElementPresent(By by, String error_message)
+    {
+        return waitForElementPresent(by, error_message, 5);
+
+    }
+    private WebElement waitForElementAndClick(By by,String error_message, long timeInSec)
+    {
+        WebElement element = waitForElementPresent(by, error_message, timeInSec);
+        element.click();
+        return element;
+    }
+
+
 }
